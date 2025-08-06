69c9ac13c6300263b869e93446faf4cf695233f9
nikborovikov <nikborovikov@team.amocrm.com>
2025-08-06 19:49:55 +0300

    corrected GORM tegs

--- internal/core/amocrm/account.go
+++ internal/core/amocrm/account.go
@@ -3,12 +3,12 @@ package amocrm
 import "time"
 
 type Account struct {
-	AccountID    int `gorm:"primaryKey"`
-	Domain       string
-	AccessToken  string
-	RefreshToken string
-	Expires      int64
-	IssuedAt     time.Time
-	UnisenderKey string
+	AccountID    int       `gorm:"primaryKey"`
+	Domain       string    `gorm:"type:varchar(255);unique"`
+	AccessToken  string    `gorm:"type:text"`
+	RefreshToken string    `gorm:"type:text"`
+	Expires      int64     `gorm:"type:int"`
+	IssuedAt     time.Time `gorm:"type:datetime"`
+	UnisenderKey string    `gorm:"type:text"`
 	Contacts     []Contact `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE"`
 }
--- internal/core/amocrm/contact.go
+++ internal/core/amocrm/contact.go
@@ -1,9 +1,9 @@
 package amocrm
 
 type Contact struct {
-	ContactID  int `gorm:"primaryKey"`
-	AccountID  int
-	Name       string
-	Email      string
-	SyncStatus bool
+	ContactID  int    `gorm:"primaryKey"`
+	AccountID  int    `gorm:"type:bigint"`
+	Name       string `gorm:"type:varchar(255)"`
+	Email      string `gorm:"type:varchar(255)"`
+	SyncStatus bool   `gorm:"type:tinyint(1)"`
 }
